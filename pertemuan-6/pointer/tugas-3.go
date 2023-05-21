b1702f1f5d892b7e51cffe3b1e3553fa84ef6ef7
Golfantara <golfantara7@gmail.com>
2023-05-21 14:44:28 +0700

    tugas-3-pointer

---
+++ pertemuan-6/pointer/tugas-3.go
@@ -0,0 +1,45 @@
+/*
+buatlah function yang menambahkan data di bawah ini ke variabel buah (wajib menggunakan
+pointer):
+● Jeruk
+● Semangka
+● Mangga
+● Strawberry
+● Durian
+● Manggis
+● Alpukat
+lalu tampilkan dengan loop dan beri angka di depannya sehingga menghasilkan output seperti
+ini:
+1. Jeruk
+2. Semangka
+3. Mangga
+4. Strawberry
+5. Durian
+6. Manggis
+7. Alpukat
+
+*/
+
+package main
+
+import "fmt"
+
+func addToBuah(slice *[]string, elements string) {
+	*slice = append(*slice, elements)
+}
+
+func main() {
+	var buah = []string{}
+
+	addToBuah(&buah, "Jeruk")
+	addToBuah(&buah, "Semangka")
+	addToBuah(&buah, "Mangga")
+	addToBuah(&buah, "Strawberry")
+	addToBuah(&buah, "Durian")
+	addToBuah(&buah, "Manggis")
+	addToBuah(&buah, "Alpukat")
+
+	for i, item := range buah {
+		fmt.Println(i+1, item)
+	}
+}
