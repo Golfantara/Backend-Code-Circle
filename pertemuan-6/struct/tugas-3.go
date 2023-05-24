8157f5f3d913c5f3a75bf3270c3d11f15c90a426
Golfantara <golfantara7@gmail.com>
2023-05-24 13:52:09 +0700

    tugas-3-struct

---
+++ pertemuan-6/struct/tugas-3.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type phone struct {
+	name, brand string
+	year        int
+	colors      []string
+}
+
+func (p *phone) addColor(color string) {
+	p.colors = append(p.colors, color)
+}
+
+func main() {
+	myPhone := phone{
+		name:  "iphone x",
+		brand: "apple",
+		year:  2018,
+		colors: []string{
+			"red",
+			"blue,",
+			"black,",
+		},
+	}
+
+	fmt.Println("name :", myPhone.name)
+	fmt.Println("brand :", myPhone.brand)
+	fmt.Println("year :", myPhone.year)
+	fmt.Println("colors :", myPhone.colors)
+}
